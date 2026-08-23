590cfa060a41a88dc71c132c8766c95181460899
Farhan-mohammad-shaikh <farhanmohammadshaikh@gmail.com>
2026-08-23 12:48:48 +0200

    Unit test for vibration processing function

--- tests/test_vibration_processing.c
+++ tests/test_vibration_processing.c
@@ -4,27 +4,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+static bool Test_CalculateRms_ValidSamples(void)
 {
     int16_t samples[] = {100, -100, 200, -200};
     uint32_t rmsMg = 0;
 
-    if (!VibrationProcessing_CalculateRms(
-            samples,
-            4U,
-            &rmsMg))
+    if (!VibrationProcessing_CalculateRms(samples, 4U, &rmsMg))
     {
-        printf("FAIL: calculation returned false.\n");
-        return EXIT_FAILURE;
+        printf("FAIL: valid RMS calculation returned false.\n");
+        return false;
     }
 
     if (rmsMg != 158U)
     {
         printf("FAIL: expected 158, got %u.\n", rmsMg);
+        return false;
+    }
+
+    return true;
+}
+
+static bool Test_CalculateRms_NullSamples(void)
+{
+    uint32_t rmsMg = 0;
+
+    if (VibrationProcessing_CalculateRms(NULL, 4U, &rmsMg))
+    {
+        printf("FAIL: NULL samples should return false.\n");
+        return false;
+    }
+
+    return true;
+}
+
+
+
+int main(void)
+{
+    if (!Test_CalculateRms_ValidSamples())
+    {
         return EXIT_FAILURE;
     }
 
-    printf("PASS: RMS calculation test.\n");
+    if (!Test_CalculateRms_NullSamples())
+    {
+        return EXIT_FAILURE;
+    }
+
+    printf("PASS: all vibration processing tests.\n");
 
     return EXIT_SUCCESS;
 }
